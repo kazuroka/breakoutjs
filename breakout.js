c1b52fe7677cdc84459d4a848526ef1f6777f8ec
Nicholas Townsend <dechande@gmail.com>
2018-10-01 00:58:06 -0400

    Added paddle, basic edge collision for paddle, and input for paddle.

--- breakout.js
+++ breakout.js
@@ -6,6 +6,42 @@ var x = canvas.width / 2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth)/2;
+var paddleY = canvas.height - paddleHeight;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+function handleInput() {
+    if(rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+}
 
 function collisionCheck() {
     if(y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
@@ -16,6 +52,13 @@ function collisionCheck() {
     }
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#9500DD";
+    ctx.fill();
+    ctx.closePath();
+}
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -26,9 +69,12 @@ function drawBall() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPaddle();
     drawBall();
     x += dx;
     y += dy;
     collisionCheck();
+    handleInput();
 }
+
 setInterval(draw, 16.666);
