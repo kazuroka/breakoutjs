f656a330855e2c4bb3fd0bd0ded4e6df91479594
Nicholas Townsend <dechande@gmail.com>
2018-09-30 23:50:46 -0400

    Added draw loop and ball drawing function. Added frame limit to 60fps.

--- breakout.js
+++ breakout.js
@@ -1,9 +1,23 @@
 var canvas = document.getElementById("gameCanvas");
-var gameWnd = canvas.getContext("2d");
+var ctx = canvas.getContext("2d");
 
-gameWnd.beginPath();
-gameWnd.rect(20, 40, 50, 50);
-gameWnd.fillStyle = "#FF0000";
-gameWnd.fill();
-gameWnd.closePath();
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
 
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+setInterval(draw, 16.666);
